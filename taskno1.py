68422cd6b4d8e0079ac81c346a4e38f6f2e757b7
Ekrash Zahid <mekrashzahid@gmail.com>
2026-06-19 11:04:21 +0500

    input taken

--- taskno1.py
+++ taskno1.py
@@ -1,1 +1,8 @@
+print('---------------------')
 print("Student Grade System")
+print("---------------------")
+name=input("Enter student name:")
+class1=int(input("Enter the name of class:"))
+marks=int(input("Enter the marks of subject physics:"))
+
+
