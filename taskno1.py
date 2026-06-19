60d0c3324e02b764119064c4730ead2831b1e9ee
Ekrash Zahid <mekrashzahid@gmail.com>
2026-06-19 11:33:13 +0500

    Done without functions

--- taskno1.py
+++ taskno1.py
@@ -1,27 +1,35 @@
+
 print('---------------------')
 print("-Student Grade System-")
 print("---------------------")
 noofstudent=int(input("Enter the total number of students:"))
 name=input("Enter student name:")
 class1=int(input("Enter the name of class:"))
-marks=int(input("Enter the marks of subject physics:"))
+subject1=input("Enter the subject:")
+subject2=input("Enter the subject:")
+subject3=input("Enter the subject:")
+marks1=int(input("Enter the marks of subject physics:"))
+marks2=int(input("Enter the marks of subject chemistry:"))
+marks3=int(input("Enter the marks of subject maths:"))
+avarage=(marks1+marks2+marks3)/3
+print("The average marks of student is:",avarage)
 
 
-if(marks<0 or marks>100):
+
+if(avarage<0 or avarage>100):
     print("Invalid marks. Please enter a value between 0 and 100.")
 
-
-elif marks>90:
+elif avarage>90:
     print("Grade A")
 
-elif marks>=80 and marks<90:
+elif avarage>=80 and avarage<90:
     print("Grade B")
 
-elif marks>70 and marks<=80:
+elif avarage>70 and avarage<=80:
     print("Grade C")
 
-elif marks>60 and marks<70:
+elif avarage>60 and avarage<70:
     print("Grade D")
 
-elif marks>50 and marks<=60:
+elif avarage>50 and avarage<=60:
     print("Grade F")
