832de5fd351667e72b3238dc7e4acd0464629c90
Ekrash Zahid <mekrashzahid@gmail.com>
2026-06-19 11:38:21 +0500

    task completed

--- taskno1.py
+++ taskno1.py
@@ -2,24 +2,22 @@
 print('---------------------')
 print("-Student Grade System-")
 print("---------------------")
-noofstudent=int(input("Enter the total number of students:"))
+
 name=input("Enter student name:")
 class1=int(input("Enter the name of class:"))
+
 subject1=input("Enter the subject:")
 subject2=input("Enter the subject:")
 subject3=input("Enter the subject:")
-marks1=int(input("Enter the marks of subject physics:"))
-marks2=int(input("Enter the marks of subject chemistry:"))
-marks3=int(input("Enter the marks of subject maths:"))
+
+marks1=int(input(f"Enter the marks of subject {subject1}:"))
+marks2=int(input(f"Enter the marks of subject {subject2}:"))
+marks3=int(input(f"Enter the marks of subject {subject3}:"))
 avarage=(marks1+marks2+marks3)/3
 print("The average marks of student is:",avarage)
 
 
-
-if(avarage<0 or avarage>100):
-    print("Invalid marks. Please enter a value between 0 and 100.")
-
-elif avarage>90:
+if avarage>90:
     print("Grade A")
 
 elif avarage>=80 and avarage<90:
